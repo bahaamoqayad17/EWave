72d6e307af0bb7c81b1632c0f7ad8e9413892d8f
Bahaa Moqayad <bahaa200030.m7taref@gmail.com>
2023-09-18 16:07:16 +0300

    test

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 const User = require("../Models/User");
 const CatchAsync = require("../utils/CatchAsync");
 const AppError = require("../utils/AppError");
+const uuid = require("uuid");
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -26,10 +27,11 @@ const createSendToken = (user, statusCode, req, res) => {
 };
 
 exports.register = CatchAsync(async (req, res, next) => {
+  const mobileNumber = uuid.v4();
   const user = await User.create({
     // name: req.body.name,
     email: req.body.email,
-    // mobile_number: req.body.mobile_number,
+    mobile_number: mobileNumber,
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
   });
